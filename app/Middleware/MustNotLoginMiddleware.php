657956e2f82a55689d209d7b3bbd63193e47fe99
Thoriq <155645840+thoriqnst0610@users.noreply.github.com>
2024-08-26 16:27:27 +0700

    Add files via upload

---
+++ app/Middleware/MustNotLoginMiddleware.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace laundry\Middleware;
+
+use laundry\App\View;
+use laundry\Config\Database;
+use laundry\Repository\SessionRepository;
+use laundry\Repository\UserRepository;
+use laundry\Service\SessionService;
+
+class MustNotLoginMiddleware implements Middleware
+{
+    private SessionService $sessionService;
+
+    public function __construct()
+    {
+        $sessionRepository = new SessionRepository(Database::getConnection());
+        $userRepository = new UserRepository(Database::getConnection());
+        $this->sessionService = new SessionService($sessionRepository, $userRepository);
+    }
+
+    function before(): void
+    {
+        $user = $this->sessionService->current();
+        if ($user != null) {
+            View::redirect('/');
+        }
+    }
+}
